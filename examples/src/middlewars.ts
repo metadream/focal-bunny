f548035cad22c2e83b5be8f14fe18a4053450951
metadream <app@zerg.cc>
2026-05-12 18:48:22 +0800

    remove examples

--- examples/src/middlewars.ts
+++
@@ -1,8 +0,0 @@
-import { type Context } from "../../bunny";
-
-export default async (c: Context, next: () => Promise<void>) => {
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${c.req.method} ${new URL(c.req.url).pathname} ${ms}ms`);
-};
